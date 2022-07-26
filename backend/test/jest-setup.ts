71e1ee8b41b4b3dd37bfe06e85977e0c96134dfa
JVSRamos <joaoramosj@usp.br>
2022-07-26 13:38:39 -0300

    feat: Add module alias

--- backend/test/jest-setup.ts
+++
@@ -1,8 +0,0 @@
-import { SetupServer } from '@src/index';
-import supertest from 'supertest';
-
-beforeAll(() => {
-    const server = new SetupServer();
-    server.init();
-    global.testRequest = supertest(server.getApp());
-});
