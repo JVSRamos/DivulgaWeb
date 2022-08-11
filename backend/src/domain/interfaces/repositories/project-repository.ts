ac45c815f8a09d8b09e5dfa02c28cf76913f17d9
JVSRamos <joaoramosj@usp.br>
2022-08-11 15:29:57 -0300

    refact: Change return of create Project function

--- backend/src/domain/interfaces/repositories/project-repository.ts
+++ backend/src/domain/interfaces/repositories/project-repository.ts
@@ -3,5 +3,5 @@ import { ProjectEntity } from '@src/domain/entities/project-entity';
 export interface ProjectRepository {
   getProjects(): Promise<ProjectEntity[]>;
 
-  createProject(project: ProjectEntity): Promise<boolean>;
+  createProject(project: ProjectEntity): Promise<ProjectEntity>;
 }
